ad443c14908fce11b3e98994ade6b5c80a210dc7
SIMPARA-Mohamed-EL-Bechir <simparamohamedelbechir7.com>
2024-11-21 10:17:30 +0100

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-import numpy as np
+import os
 from flask import Flask, request, jsonify
+import numpy as np
 import joblib
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Permet les requêtes CORS depuis n'importe quelle origine
+CORS(app)
 
 # Charger le modèle enregistré
 model = joblib.load('best_model_logreg.pkl')
@@ -15,32 +16,25 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Extraire les données envoyées dans la requête POST
     data = request.get_json()
     
-    # Vérifier que toutes les données nécessaires sont présentes
     required_fields = ['menopaus', 'agegrp', 'density', 'race', 
                        'bmi', 'agefirst', 'nrelbc', 'hrt']
-    
     for field in required_fields:
         if field not in data:
             return jsonify({'error': f'Missing field: {field}'}), 400
 
-    # Extraire les caractéristiques de l'utilisateur et les transformer en tableau numpy
     features = np.array([data['menopaus'], data['agegrp'], data['density'], data['race'], 
                          data['bmi'], data['agefirst'], data['nrelbc'], data['hrt']]).reshape(1, -1)
 
-    # Obtenir les probabilités avec predict_proba
     probabilities = model.predict_proba(features)
-    
-    # La probabilité de la classe "cancer" (1)
     cancer_probability = probabilities[0][1]
-    
-    # Retourner la probabilité au format JSON
+
     return jsonify({
         'probability_cancer': float(cancer_probability),
         'prediction': int(model.predict(features)[0])
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
